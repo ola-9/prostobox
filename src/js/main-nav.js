6b1ab2f9d12fd4cae71d624994e99af664fb5be3
Dmitry Iovev <iovlev.d@gmail.com>
2021-05-30 11:11:11 +0300

    Добавляет мобильное меню

--- src/js/main-nav.js
+++ src/js/main-nav.js
@@ -1,2 +1,13 @@
-console.log('test')
-alert('xxx')
+(function() {
+  'use strict;'
+
+  const nav = document.querySelector('.main-nav');
+  const navToggle = document.querySelector('.nav-toggle');
+
+  navToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    navToggle.classList.toggle('nav-toggle--opened');
+    nav.classList.toggle('main-nav--opened');
+  });
+
+})();
