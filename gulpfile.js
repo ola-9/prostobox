83c54e945fdc628d88a66a8e05b13c2e9ed1f265
Dmitry Iovev <iovlev.d@gmail.com>
2021-06-12 12:47:31 +0300

    Исправляет сборку и шаблоны

--- gulpfile.js
+++ gulpfile.js
@@ -8,64 +8,63 @@ const sync = require('browser-sync').create();
 const csso = require('gulp-csso');
 const terser = require('gulp-terser');
 const concat = require('gulp-concat');
-// const webp = require('gulp-webp');
 const del = require('del');
 const rename = require('gulp-rename');
 const svgstore = require('gulp-svgstore');
 
 const nunjucks = require('gulp-nunjucks-render');
 
-const stylesMin = () => {
-  return gulp.src('src/scss/style.scss')
+const styles = () => {
+  return gulp.src('./src/scss/style.scss')
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('./build/css'))
     .pipe(postcss([
       autoprefixer()
     ]))
     .pipe(csso())
     .pipe(rename('style.min.css'))
     .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest('./build/css'))
     .pipe(sync.stream());
 }
 
-exports.stylesMin = stylesMin;
-
 const scripts = () => {
-  return gulp.src('src/js/**/*.js')
+  return gulp.src('./src/js/**/*.js')
     .pipe(concat('script.js'))
+    .pipe(gulp.dest('./build/js'))
     .pipe(terser())
     .pipe(rename('script.min.js'))
-    .pipe(gulp.dest('build/js'));
+    .pipe(gulp.dest('./build/js'));
 }
 
-exports.scripts = scripts;
+// const html = () => {
+//   return gulp.src('src/*.html')
+//   .pipe(gulp.dest('build'));
+// }
 
-const html = () => {
-  return gulp.src('src/*.html')
-  .pipe(gulp.dest('build'));
-}
-
-exports.html = html;
+// exports.html = html;
 
 const render = () => {
-  return gulp.src('src/pages/*.html')
+  return gulp.src('./src/pages/**/*.html')
+  // .pipe(
+  //   data(() => {
+  //     return require("./app/data.json");
+  //   })
+  // )
   .pipe(nunjucks({
-      path: ['src/templates']
+      path: ['./src/templates']
     }))
-  .pipe(gulp.dest('build'));
+  .pipe(gulp.dest('./build'));
 }
-
-exports.render = render;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'build'
+      baseDir: './build'
     },
     cors: true,
     notify: false,
@@ -74,69 +73,63 @@ const server = (done) => {
   done();
 }
 
-exports.server = server;
-
 const reload = (done) => {
   sync.reload();
   done();
 }
 
-exports.reload = reload;
-
 const sprite = () => {
-  return gulp.src('src/img/icons/**/*.svg')
+  return gulp.src('./src/img/icons/**/*.svg')
     .pipe(svgstore({inlineSvg: true}))
     .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img'));
+    .pipe(gulp.dest('./build/img'));
 }
 
-exports.sprite = sprite;
+const clean = () => {
+  return del('./build');
+}
+
+const cleanDocs = () => {
+  return del('./docs');
+}
+
+const copy = () => {
+  return gulp.src([
+    // 'src/*.ico',
+    './src/fonts/**/*.{woff,woff2}',
+    './src/img/**/*.{png,jpg,svg}'
+  ], {
+    base: 'src'
+  }).pipe(gulp.dest('./build'));
+}
+
+const copyImages = () => {
+  return gulp.src([
+    './src/img/**/*.{png,jpg,svg}'
+  ], {
+    base: 'src'
+  }).pipe(gulp.dest('./build'));
+}
 
 // Watcher
 
 const watcher = () => {
-  gulp.watch('src/scss/**/*.scss', gulp.series('stylesMin'));
-  gulp.watch('src/img/icons/*.svg', gulp.series('sprite', 'reload'));
-  // gulp.watch('src/img/**/*.{png,jpg,svg}', gulp.series('copy', 'reload'));
-  gulp.watch(['src/js/*.js', 'src/js/vendor/*.js'], gulp.series('scripts', 'reload'));
-  gulp.watch(['src/pages/**/*.html', 'src/templates/**/*.html'], gulp.series('render', 'reload'));
+  gulp.watch(['./src/scss/**/*.scss'], gulp.series(styles));
+  gulp.watch(['./src/img/icons/*.svg'], gulp.series(sprite, reload));
+  gulp.watch(['./src/img/**/*.{png,jpg,svg}'], gulp.series(copyImages, reload));
+  gulp.watch(['./src/js/*.js', '!./src/js/vendor/*.js'], gulp.series(scripts, reload));
+  gulp.watch(['./src/pages/**/*.html', './src/templates/**/*.html'], gulp.series(render, reload));
 }
-
-const clean = () => {
-  return del('build');
-}
-
-exports.clean = clean;
-
-const cleanDocs = () => {
-  return del('docs');
-}
-
-exports.clean = clean;
-
-const copy = () => {
-  return gulp.src([
-    'src/*.ico',
-    'src/fonts/**/*.{woff,woff2}',
-    'src/img/**'
-  ], {
-    base: 'src'
-  }).pipe(gulp.dest('build/'));
-}
-
-exports.copy = copy;
 
 const docs = () => {
-  return gulp.src('build/**',
+  return gulp.src('./build/**',
   {
     base: 'build'
-  }).pipe(gulp.dest('docs'));
+  }).pipe(gulp.dest('./docs'));
 }
 
-exports.docs = docs;
-
 const build = gulp.series(
-  clean, copy, stylesMin, scripts, render, sprite
+  clean, copy, styles, scripts, render, sprite
 );
 
 exports.build = build;
