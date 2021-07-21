0b10532d419a41a29089cfa12c4b8149d372345e
Dmitry Iovev <iovlev.d@gmail.com>
2021-07-21 10:41:33 +0300

    Bug fixes

--- src/js/how-to-by.js
+++ src/js/how-to-by.js
@@ -10,6 +10,7 @@
   const optionNo= document.querySelector('.delivery-options--no');
 
   const deliveryOptions = document.querySelector('.delivery-options--no');
+
   const btnFast = document.querySelector('.js-delivery-btn-fast');
   const btnSimple = document.querySelector('.js-delivery-btn-simple');
   let btnOptCurrent = document.querySelector('.js-delivery-btn-current');
@@ -37,51 +38,55 @@
     }
   }
 
-  pricingOptions.addEventListener('click', (e)=> {
-    e.preventDefault();
-    if (e.target === btnCurrent) return;
+  if (pricingOptions) {
+    pricingOptions.addEventListener('click', (e)=> {
+      e.preventDefault();
+      if (e.target === btnCurrent) return;
 
-    if (e.target === btnYes) {
-      btnCurrent = btnYes;
-      pricingOptions.classList.remove('pricing-options--no');
-      pricingOptions.classList.add('pricing-options--yes');
-      btnYes.classList.remove('button--white');
-      btnNo.classList.add('button--white');
-      optionYes.style.display = 'block';
-      optionNo.style.display = 'none';
-    }
+      if (e.target === btnYes) {
+        btnCurrent = btnYes;
+        pricingOptions.classList.remove('pricing-options--no');
+        pricingOptions.classList.add('pricing-options--yes');
+        btnYes.classList.remove('button--white');
+        btnNo.classList.add('button--white');
+        optionYes.style.display = 'block';
+        optionNo.style.display = 'none';
+      }
 
-    if (e.target === btnNo) {
-      btnCurrent = btnNo;
-      pricingOptions.classList.remove('pricing-options--yes');
-      pricingOptions.classList.add('pricing-options--no');
-      btnNo.classList.remove('button--white');
-      btnYes.classList.add('button--white');
-      optionYes.style.display = 'none';
-      optionNo.style.display = 'block';
-    }
+      if (e.target === btnNo) {
+        btnCurrent = btnNo;
+        pricingOptions.classList.remove('pricing-options--yes');
+        pricingOptions.classList.add('pricing-options--no');
+        btnNo.classList.remove('button--white');
+        btnYes.classList.add('button--white');
+        optionYes.style.display = 'none';
+        optionNo.style.display = 'block';
+      }
 
-  });
+    });
+  }
 
-  deliveryOptions.addEventListener('click', (e)=> {
-    e.preventDefault();
+  if (deliveryOptions) {
+    deliveryOptions.addEventListener('click', (e)=> {
+      e.preventDefault();
 
-    if (e.target === btnOptCurrent) return;
+      if (e.target === btnOptCurrent) return;
 
-    if (e.target === btnFast) {
+      if (e.target === btnFast) {
 
-      btnOptCurrent = btnFast;
-      btnFast.classList.remove('button--white');
-      btnSimple.classList.add('button--white');
-      showFastOptions();
-    }
+        btnOptCurrent = btnFast;
+        btnFast.classList.remove('button--white');
+        btnSimple.classList.add('button--white');
+        showFastOptions();
+      }
 
-    if (e.target === btnSimple) {
-      btnOptCurrent = btnSimple;
-      btnSimple.classList.remove('button--white');
-      btnFast.classList.add('button--white');
-      showSimpleOptions();
-    }
-  });
+      if (e.target === btnSimple) {
+        btnOptCurrent = btnSimple;
+        btnSimple.classList.remove('button--white');
+        btnFast.classList.add('button--white');
+        showSimpleOptions();
+      }
+    });
+  }
 
 })();
